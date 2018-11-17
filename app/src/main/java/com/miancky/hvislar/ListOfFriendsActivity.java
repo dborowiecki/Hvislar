8345135ac64f401588c47333f91ad0b07907286e
dmn <dborowiecki7@gmail.com>
2018-11-17 12:50:22 +0100

    Added friend list activity

---
+++ app/src/main/java/com/miancky/hvislar/ListOfFriendsActivity.java
@@ -0,0 +1,13 @@
+package com.miancky.hvislar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListOfFriendsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_of_friends);
+    }
+}

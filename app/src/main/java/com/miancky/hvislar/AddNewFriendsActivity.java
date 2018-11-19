d0b5227653df19e9f37b20ee177d337231072f8e
dmn <dborowiecki7@gmail.com>
2018-11-19 20:44:57 +0100

    List of users is displaying

--- app/src/main/java/com/miancky/hvislar/AddNewFriendsActivity.java
+++ app/src/main/java/com/miancky/hvislar/AddNewFriendsActivity.java
@@ -1,16 +1,13 @@
 package com.miancky.hvislar;
 
-import android.app.ListActivity;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -29,80 +26,85 @@ import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+interface VolleyCallback{
+    void onSuccess(List<String> result);
+}
+public class AddNewFriendsActivity extends AppCompatActivity {
 
-public class ListOfFriendsActivity extends AppCompatActivity {
-
+    List<String> userList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_of_friends);
+        setContentView(R.layout.activity_add_new_friends);
 
-        Intent intent = getIntent();
-        String name = intent.getStringExtra("name");
-        String email = intent.getStringExtra("email");
+       Intent intent = getIntent();
+       String name = intent.getStringExtra("name");
+       String email = intent.getStringExtra("email");
 
-        ListView listOfFriends = (ListView)findViewById(R.id.lvFriends);
+        ListView listOfFriends = (ListView)findViewById(R.id.lvUsers);
         // create the ArrayList to store the titles of nodes
-        ArrayList<String> listItems = new ArrayList<String>();
+        final ArrayList<String> listItems = new ArrayList<String>();
         //TODO: should take friends from db
-        listItems.add("Test1");
-        listItems.add("Test2");
-        ArrayAdapter ad = new ArrayAdapter(ListOfFriendsActivity.this,
-                android.R.layout.simple_list_item_1, listItems);
+        listItems.add("test");
+        fetchUsersFromDb(null);
+        getUserList(new VolleyCallback() {
+            @Override
+            public void onSuccess(List<String> result) {
+                listItems.addAll(result);
+                //Toast.makeText(AddNewFriendsActivity.this, userList.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+        listItems.addAll(userList);
+        UserListAdapter ad = new UserListAdapter(AddNewFriendsActivity.this, listItems);
 
         // give adapter to ListView UI element to render
         listOfFriends.setAdapter(ad);
 
-        listOfFriends.setOnItemClickListener(new AdapterView.OnItemClickListener()
-        {
-            //TODO: should open conversation with friend
-            @Override
-            public void onItemClick(AdapterView<?> arg0, View arg1,int position, long arg3)
-            {
-                Toast.makeText(ListOfFriendsActivity.this, "" + position, Toast.LENGTH_SHORT).show();
-            }
-        });
     }
-
-    public void goToUserProfile(View view){
-        Intent intent = new Intent(ListOfFriendsActivity.this, UserProfile.class);
+    public void goToFriendList(View view){
+        Intent intent = new Intent(AddNewFriendsActivity.this, ListOfFriendsActivity.class);
         intent.putExtra("name", getIntent().getStringExtra("name"));
         intent.putExtra("email", intent.getStringExtra("email"));
         startActivity(intent);
     }
 
-    public void goToAddingFriends(View view){
-        Intent intent = new Intent(ListOfFriendsActivity.this, AddNewFriendsActivity.class);
-        intent.putExtra("name", getIntent().getStringExtra("name"));
-        intent.putExtra("email", intent.getStringExtra("email"));
-        startActivity(intent);
+    public void refresh(View view){
+       // getUserList(userList);
+        Toast.makeText(AddNewFriendsActivity.this, userList.toString(), Toast.LENGTH_LONG).show();
+
+        Toast.makeText(AddNewFriendsActivity.this, "refreshing", Toast.LENGTH_LONG).show();
     }
-    public void getListOfFriends(View view){
+
+    public void fetchUsersFromDb(View view){
+     //   getUserList(userList);
+    }
+    public void getUserList(final VolleyCallback callback){
         try {
             AssetManager am = getApplicationContext().getAssets();
             InputStream is = am.open("private/CONFIG");
+            final List<String> users = new LinkedList<>();
             final String LOGIN_URL = convert(is, Charset.defaultCharset())+"getAllUsers.php";
 
             StringRequest stringRequest = new StringRequest(Request.Method.POST, LOGIN_URL,
                     new Response.Listener<String>() {
+                        List<String> recivedUsers = new ArrayList<>();
                         @Override
                         public void onResponse(String response) {
-                            Toast.makeText(ListOfFriendsActivity.this,response,Toast.LENGTH_LONG).show();
                             try {
                                 JSONObject JSONresponse = new JSONObject(response);
                                 if(!JSONresponse.getBoolean("success")){
-                                    Toast.makeText(ListOfFriendsActivity.this,"Fetching friends list failed",Toast.LENGTH_LONG).show();
+                                    Toast.makeText(AddNewFriendsActivity.this,"Fetching friends list failed",Toast.LENGTH_LONG).show();
                                 }
                                 else{
                                     JSONArray friends = JSONresponse.getJSONArray("users");
-                                    List<String> list = new ArrayList<String>();
                                     for(int i = 0; i < friends.length(); i++){
-                                        list.add(friends.getJSONObject(i).getString("name"));
+                                        recivedUsers.add(friends.getString(i));
                                     }
-                                    Toast.makeText(ListOfFriendsActivity.this,list.toString(),Toast.LENGTH_LONG).show();
+                                    callback.onSuccess(recivedUsers);
                                 }
                             } catch (JSONException e) {
                                 e.printStackTrace();
@@ -113,7 +115,7 @@ public class ListOfFriendsActivity extends AppCompatActivity {
                     new Response.ErrorListener() {
                         @Override
                         public void onErrorResponse(VolleyError error) {
-                            Toast.makeText(ListOfFriendsActivity.this,error.toString()+LOGIN_URL,Toast.LENGTH_LONG).show();
+                            Toast.makeText(AddNewFriendsActivity.this,error.toString()+LOGIN_URL,Toast.LENGTH_LONG).show();
                         }
                     }){
                 @Override
@@ -128,8 +130,9 @@ public class ListOfFriendsActivity extends AppCompatActivity {
             RequestQueue requestQueue = Volley.newRequestQueue(this);
             requestQueue.add(stringRequest);
         } catch (Exception e){
-            Toast.makeText(ListOfFriendsActivity.this,e.toString(),Toast.LENGTH_LONG).show();
+            Toast.makeText(AddNewFriendsActivity.this,e.toString(),Toast.LENGTH_LONG).show();
         }
+
     }
     //TODO: refactor, move to another class
     public String convert(InputStream inputStream, Charset charset) throws IOException {

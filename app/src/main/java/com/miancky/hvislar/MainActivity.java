bd55756cda9829273d2fe1982a11732220e10377
JBladejs <JBladejs@gmail.com>
2018-11-20 04:38:39 +0100

    Changed Homescreen

--- app/src/main/java/com/miancky/hvislar/MainActivity.java
+++ app/src/main/java/com/miancky/hvislar/MainActivity.java
@@ -1,6 +1,7 @@
 package com.miancky.hvislar;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +10,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ActionBar ab=getSupportActionBar();
+        if(ab!=null)
+            ab.setElevation(0);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }

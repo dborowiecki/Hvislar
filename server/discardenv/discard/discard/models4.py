ccab035089822037cafbc4b0d77657b87b652966
dmn <dborowiecki7@gmail.com>
2018-12-10 23:05:54 +0100

    Deleted redundant files

--- server/discardenv/discard/discard/models4.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.template.defaultfilters import slugify
-from django.contrib.auth.models import User
-
-class Conversation(models.Model):
-    id_conversation_pk = models.AutoField(primary_key=True)
-  
-    class Meta:
-db_table = '"Conversation"'

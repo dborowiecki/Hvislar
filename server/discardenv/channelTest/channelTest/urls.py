eb9073fdcd295adb28fb8b0a70ded336fb8e0d1e
dmn <dborowiecki7@gmail.com>
2018-12-22 13:55:26 +0100

    Basic html template with chat room added

--- server/discardenv/channelTest/channelTest/urls.py
+++ server/discardenv/channelTest/channelTest/urls.py
@@ -1,4 +1,4 @@
-"""channelsTest URL Configuration
+"""channelTest URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# mysite/urls.py
+from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^chat/', include('chat.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
--- server/discardenv/channelTest/manage.py
+++ server/discardenv/channelTest/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'channelsTest.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'channelTest.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

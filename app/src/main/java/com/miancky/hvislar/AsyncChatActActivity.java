125f3fe42469e5929a6b62103d960d6cb0c2a960
dmn <dborowiecki7@gmail.com>
2018-12-25 20:01:25 +0100

    Added async chat activity

---
+++ app/src/main/java/com/miancky/hvislar/AsyncChatActActivity.java
@@ -0,0 +1,13 @@
+package com.miancky.hvislar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AsyncChatActActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_async_chat_act);
+    }
+}

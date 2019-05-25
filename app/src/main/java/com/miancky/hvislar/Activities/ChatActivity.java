0c7885f44a8bf5cf5f358527b8999e7227a74c48
JBladejs <JBladejs@gmail.com>
2019-05-25 02:48:48 +0200

    Refactoring

--- app/src/main/java/com/miancky/hvislar/Activities/ChatActivity.java
+++ app/src/main/java/com/miancky/hvislar/Activities/ChatActivity.java
@@ -7,14 +7,6 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.miancky.hvislar.Complementary.MessageAdapter;
 import com.miancky.hvislar.R;
 
@@ -27,7 +19,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class ChatActivity extends AppCompatActivity {
+import static com.miancky.hvislar.ServerCommunication.ServerCommunicator.sendRequest;
+
+public class ChatActivity extends ResponsiveActivity {
+
+    boolean sending;
+    private List<String> msgs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,93 +32,30 @@ public class ChatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_chat);
         Intent intent = getIntent();
         setTitle(intent.getStringExtra("friendName"));
+        msgs= new ArrayList<>();
         }
 
     public void sendMessage(View view){
-        final String SEND_MESSAGE_URL = "http://" + getString(R.string.ip) +  getString(R.string.port) + "/sendMessage/";
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, SEND_MESSAGE_URL,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject JSONResponse = new JSONObject(response);
-                            if (!JSONResponse.getBoolean("success"))
-                                Toast.makeText(ChatActivity.this, "Cannot send message!", Toast.LENGTH_LONG).show();
-                            else
-                                establishServerConnection();
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(ChatActivity.this, error.toString(), Toast.LENGTH_LONG).show();
-                    }
-                }) {
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<>();
-                EditText editText = findViewById(R.id.sendEditText4);
-                Intent intent = getIntent();
-                params.put("email", intent.getStringExtra("email"));
-                params.put("password", intent.getStringExtra("password"));
-                params.put("message", editText.getText().toString());
-                params.put("reciver_name", intent.getStringExtra("friendName"));
-                return params;
-            }
-        };
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(stringRequest);
-    }
-
-    private void establishServerConnection(View view){
-        establishServerConnection();
+        sending = true;
+        Map<String, String> params = new HashMap<>();
+        EditText editText = findViewById(R.id.sendEditText4);
+        Intent intent = getIntent();
+        params.put("email", intent.getStringExtra("email"));
+        params.put("password", intent.getStringExtra("password"));
+        params.put("message", editText.getText().toString());
+        params.put("reciver_name", intent.getStringExtra("friendName"));
+        sendRequest(this, getString(R.string.chat_sub_url), params);
     }
 
     public void establishServerConnection(){
-        final String GET_MESSAGES_URL = "http://" + getString(R.string.ip) +  getString(R.string.port) + "/getMessages/";
-        final List<String> msgs= new ArrayList<>();
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, GET_MESSAGES_URL,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject JSONResponse = new JSONObject(response);
-                            if (!JSONResponse.getBoolean("success"))
-                                Toast.makeText(ChatActivity.this, "Cannot download messages!", Toast.LENGTH_LONG).show();
-                            else {
-                                JSONArray receivedMessages = JSONResponse.getJSONArray("fetched_messages");
-                                for(int i = 0; i < receivedMessages.length(); i++)
-                                    msgs.add(receivedMessages.getString(i));
-                            }
-                            refreshMessages(msgs);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(ChatActivity.this, error.toString(), Toast.LENGTH_LONG).show();
-                    }
-                }) {
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<>();
-                EditText editText = findViewById(R.id.sendEditText4);
-                Intent intent = getIntent();
-                params.put("email", intent.getStringExtra("email"));
-                params.put("password", intent.getStringExtra("password"));
-                params.put("interlocutor", intent.getStringExtra("friendName"));
-                params.put("number_of_messages", "50");
-                return params;
-            }
-        };
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(stringRequest);
+        sending = false;
+        Map<String, String> params = new HashMap<>();
+        Intent intent = getIntent();
+        params.put("email", intent.getStringExtra("email"));
+        params.put("password", intent.getStringExtra("password"));
+        params.put("interlocutor", intent.getStringExtra("friendName"));
+        params.put("number_of_messages", "50");
+        sendRequest(this, getString(R.string.refresh_sub_url), params);
     }
 
     private void refreshMessages(List<String> msgs){
@@ -141,5 +75,35 @@ public class ChatActivity extends AppCompatActivity {
         ListView messagesList = findViewById(R.id.messages_view);
         MessageAdapter ad = new MessageAdapter(ChatActivity.this, messages, users);
         messagesList.setAdapter(ad);
+    }
+
+    @Override
+    public void positiveResponseReaction(JSONObject response) {
+        if(sending) establishServerConnection();
+        else{
+            try {
+                JSONArray receivedMessages = response.getJSONArray("fetched_messages");
+                for (int i = 0; i < receivedMessages.length(); i++)
+                    msgs.add(receivedMessages.getString(i));
+            }catch (JSONException e) {
+                e.printStackTrace();
+            }
+            refreshMessages(msgs);
+        }
+    }
+
+    @Override
+    public void negativeResponseReaction(JSONObject response) {
+        if(sending) Toast.makeText(ChatActivity.this, R.string.message_failure, Toast.LENGTH_LONG).show();
+        else{
+            Toast.makeText(ChatActivity.this, R.string.refresh_failure, Toast.LENGTH_LONG).show();
+            refreshMessages(msgs);
+        }
+    }
+
+    @Override
+    public void errorReaction() {
+        if(sending) Toast.makeText(ChatActivity.this, R.string.message_failure, Toast.LENGTH_LONG).show();
+        else Toast.makeText(this.getApplicationContext(),R.string.processing_error,Toast.LENGTH_LONG).show();
     }
 }
